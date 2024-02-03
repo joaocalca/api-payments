5b78acb753f76c5fa0af1dbdfc21974d4839c257
joaocalca <jvcalca@gmail.com>
2024-02-03 18:42:43 -0300

    Initial commit

---
+++ src/main/java/dev/joaocalca/ApiPaymentsApplication.java
@@ -0,0 +1,13 @@
+package dev.joaocalca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joaocalca/ApiPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joaocalca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
